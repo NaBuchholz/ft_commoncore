6d50b6b48106f4a198880d4f2bd5192950549940
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2024-10-21 14:52:45 -0300

    refactor: remove unused variable

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:57:28 by nbuchhol          #+#    #+#             */
-/*   Updated: 2024/10/21 12:39:55 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2024/10/21 14:52:23 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t			index;
 	unsigned char	*temp;
 
-	index = 0;
 	temp = malloc(nmemb * size);
 	if (!temp)
 		return (NULL);
